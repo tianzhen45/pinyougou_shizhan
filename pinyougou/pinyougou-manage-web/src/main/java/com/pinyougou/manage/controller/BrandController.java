9114a75af3ec50463914712eac1297e9c3090a98
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-01 18:32:24 +0800

    品牌分页查询实现

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -1,6 +1,7 @@
 package com.pinyougou.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.github.pagehelper.PageInfo;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,19 @@ public class BrandController {
 
     @Reference
     private BrandService brandService;
+
+    /**
+     * 分页查询品牌数据
+     * @param pageNum 页号
+     * @param pageSize 页大小
+     * @return 分页信息对象
+     */
+    @GetMapping("/findPage")
+    public PageInfo<TbBrand> findPage(@RequestParam(defaultValue = "1")Integer pageNum,
+                             @RequestParam(defaultValue = "10")Integer pageSize){
+        //return brandService.testPage(pageNum, pageSize);
+        return brandService.findPage(pageNum, pageSize);
+    }
 
     /**
      * 分页查询品牌数据
