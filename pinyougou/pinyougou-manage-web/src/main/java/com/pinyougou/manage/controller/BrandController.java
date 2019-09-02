4631ac2a3909c0e0690bf6d7bd074fe7e48c0b6c
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-02 16:25:33 +0800

    更新品牌-实现&测试

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -28,6 +28,22 @@ public class BrandController {
     }
 
     /**
+     * 更新品牌数据
+     * @param brand 品牌数据
+     * @return 操作结果
+     */
+    @PostMapping("/update")
+    public Result update(@RequestBody TbBrand brand){
+        try {
+            brandService.update(brand);
+            return Result.ok("更新品牌成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("更新品牌失败！");
+    }
+
+    /**
      * 新增品牌数据
      * @param brand 品牌数据
      * @return 操作结果
