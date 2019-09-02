f0c5e451162e35c5c241e8ca64ab6ac03056f5ad
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-02 18:15:07 +0800

    前端页面与前端js分离

---
+++ pinyougou/pinyougou-manage-web/src/main/webapp/js/controller/brandController.js
@@ -0,0 +1,96 @@
+//创建vue实例
+var app = new Vue({
+    //模版id
+    el: "#app",
+    //数据
+    data: {
+        //品牌列表
+        entityList: [],
+        //总记录数
+        total: 0,
+        //页大小
+        pageSize: 10,
+        //当前页号
+        pageNum: 1,
+        //实体
+        entity: {},
+        //选择了的id数组
+        ids: [],
+        //查询条件对象
+        searchEntity:{}
+    },
+    //方法
+    methods: {
+        //批量删除
+        deleteList: function () {
+            if (this.ids.length == 0) {
+                alert("请先选择要删除的记录！");
+                return;
+            }
+            //确认是否要删除
+            //confirm 如果点击 确定 则返回true
+            if(confirm("确定要删除选中了的那些记录吗？")){
+                axios.get("../brand/delete.do?ids=" + this.ids).then(function (response) {
+                    if(response.data.success){
+                        app.searchList(1);
+                        //清空选择的id
+                        app.ids = [];
+                    } else {
+                        alert(response.data.message);
+                    }
+                });
+            }
+        },
+        //根据id查询
+        findOne: function (id) {
+            axios.get("../brand/findOne/" + id + ".do").then(function (response) {
+                app.entity = response.data;
+            });
+        },
+        //保存数据
+        save: function () {
+            var method = "add";
+            if (this.entity.id != null) {
+                method = "update";
+            }
+            axios.post("../brand/" + method + ".do", this.entity).then(function (response) {
+                //操作结果
+                if (response.data.success) {
+                    //保存成功；刷新列表
+                    app.searchList(1);
+                } else {
+                    //保存失败，则提示
+                    alert(response.data.message);
+                }
+            });
+        },
+        //分页查询
+        searchList: function (curPage) {
+            //设置页号
+            this.pageNum = curPage;
+
+            /*axios.get("../brand/findPage.do?pageNum=" + this.pageNum + "&pageSize=" + this.pageSize).then(function (response) {
+                //response.data 分页信息对象PageInfo
+                app.entityList = response.data.list;
+                app.total = response.data.total;
+            });*/
+            axios.post("../brand/search.do?pageNum="+this.pageNum+"&pageSize=" + this.pageSize,this.searchEntity).then(function (response) {
+                //response.data 分页信息对象PageInfo
+                //记录列表
+                app.entityList = response.data.list;
+                //符合本次查询的总记录数
+                app.total = response.data.total;
+            });
+        }
+    },
+    //钩子函数
+    created() {
+        /*axios.get("../brand/findAll.do").then(function (response) {
+            //console.log(response);
+            //在axios的回调方法中必须使用app表示vue实例；this表示window所以不是vue实例
+            app.entityList = response.data;
+        });*/
+
+        this.searchList(this.pageNum);
+    }
+});
