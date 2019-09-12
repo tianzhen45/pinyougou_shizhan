7f7e4396c57d04c27ed3d9428795610a04db970b
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-12 09:57:58 +0800

    ElasticSearch-创建操作接口类及新增数据

--- itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ElasticSearchTest.java
+++ itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ElasticSearchTest.java
@@ -1,5 +1,6 @@
 package cn.itcast.elasticsearch;
 
+import cn.itcast.es.dao.ItemDao;
 import com.pinyougou.pojo.TbItem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +16,9 @@ public class ElasticSearchTest {
     @Autowired
     private ElasticsearchTemplate esTemplate;
 
+    @Autowired
+    private ItemDao itemDao;
+
     //创建索引
     @Test
     public void createIndexAndMapping(){
@@ -22,5 +26,19 @@ public class ElasticSearchTest {
         esTemplate.createIndex(TbItem.class);
         //设置映射信息
         esTemplate.putMapping(TbItem.class);
+    }
+
+    //新增或更新
+    @Test
+    public void addOrUpdate(){
+        TbItem item = new TbItem();
+        item.setId(123L);
+        item.setTitle("22222 OPPO Reno2 4800万变焦四摄 视频防抖 6.5英寸阳光护眼全面屏 8GB+128GB 深海夜光 拍照游戏智能手机");
+        item.setSeller("欧柏");
+        item.setBrand("欧柏");
+        item.setPrice(2999.0);
+        item.setCategory("手机");
+        item.setImage("https://item.jd.com/100004418727.html");
+        itemDao.save(item);
     }
 }
