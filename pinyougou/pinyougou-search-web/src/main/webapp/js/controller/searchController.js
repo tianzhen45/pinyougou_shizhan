45860de9ce5d79dafe9c5ba5703c82d49515857c
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-14 09:19:20 +0800

    过滤查询-前端过滤条件显示&撤销

--- pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -2,11 +2,35 @@ var app = new Vue({
     el:"#app",
     data:{
         //搜索条件对象
-        searchMap:{"keywords":""},
+        searchMap:{"keywords":"","brand":"","category":"", "spec":{}},
         //返回结果
         resultMap: {"itemList":[]}
     },
     methods:{
+        //移除过滤条件
+        removeSearchItem: function(key){
+            if (key == "brand" || key == "category") {
+                this.searchMap[key] = "";
+            } else {
+                //为了同步页面中的显示
+                this.$set(this.searchMap.spec, key, null);
+                //删除spec对象中某个属性
+                delete this.searchMap.spec[key];
+            }
+        },
+        //添加过滤条件对象
+        addSearchItem: function(key, value){
+            if (key == "brand" || key == "category") {
+                this.searchMap[key] = value;
+            } else {
+                //对于对象中的对象的属性设置的时候；需要使用$set
+                //参数1：要设置的对象
+                //参数2：设置对象中的属性
+                //参数3：设置对象中的属性对应的值
+                this.$set(this.searchMap.spec, key, value);
+                //this.searchMap.spec[key] = value;
+            }
+        },
         //搜索
         search:function () {
             axios.post("itemSearch/search.do", this.searchMap).then(function (response) {
