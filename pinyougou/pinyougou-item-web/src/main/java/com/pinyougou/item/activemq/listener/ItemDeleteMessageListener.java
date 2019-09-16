9749b1000acedaa917f3a0c9012f07654fec5817
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-16 12:43:30 +0800

    商品同步详情系统-删除后同步消费者实现

---
+++ pinyougou/pinyougou-item-web/src/main/java/com/pinyougou/item/activemq/listener/ItemDeleteMessageListener.java
@@ -0,0 +1,42 @@
+package com.pinyougou.item.activemq.listener;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.sellergoods.service.GoodsService;
+import com.pinyougou.sellergoods.service.ItemCatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.listener.adapter.AbstractAdaptableMessageListener;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import java.io.File;
+
+/**
+ * 接收商品spu id数组的发布与订阅消息；接收到消息之后删除指定路径下的商品详情静态html页面。
+ *
+ */
+public class TopicMessageListener extends AbstractAdaptableMessageListener {
+    @Value("${ITEM_HTML_PATH}")
+    private String ITEM_HTML_PATH;
+
+    @Override
+    public void onMessage(Message message, Session session) throws JMSException {
+        ObjectMessage objectMessage = (ObjectMessage) message;
+        //获取商品spu id数组
+        Long[] goodsIds = (Long[]) objectMessage.getObject();
+
+        //删除静态页面
+        if (goodsIds != null && goodsIds.length > 0) {
+            for (Long goodsId : goodsIds) {
+                String fileName = ITEM_HTML_PATH + goodsId + ".html";
+                File file = new File(fileName);
+                if (file.exists()) {
+                    file.delete();
+                }
+            }
+        }
+    }
+}
