9b6366ccb66610932510e173677dd91074283635
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-18 09:20:48 +0800

    Spring Boot入门

---
+++ itcast-springboot/src/main/java/cn/itcast/springboot/Application.java
@@ -0,0 +1,20 @@
+package cn.itcast.springboot;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 如果是spring boot工程则必须在启动引导类上添加
+ * SpringBootApplication 是一个组合注解
+ * 组合了ComponentScan 会扫描当前包和其子包下的spring 注解
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        /*SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);*/
+    }
+}
