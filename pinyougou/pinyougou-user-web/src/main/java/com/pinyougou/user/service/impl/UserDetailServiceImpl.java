194dbb4edb7a68b8815b8333634ebbf0e30dc43b
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-19 12:22:02 +0800

    用户中心系统整合Security&CAS

---
+++ pinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,31 @@
+package com.pinyougou.user.service.impl;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 动态认证授权
+ */
+public class UserDetailServiceImpl implements UserDetailsService {
+    /**
+     * @param username 用户在前端页面中输入的用户名
+     * @return 用户信息
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //角色权限集合
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        //因为将认证交由CAS进行；所有不需要传递密码
+        return new User(username, "", authorities);
+    }
+}
+
