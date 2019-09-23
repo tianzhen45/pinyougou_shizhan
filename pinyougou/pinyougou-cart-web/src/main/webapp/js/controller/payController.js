99006801e2279cbff3747086f0d8d5bd05252a0d
liaojianbin <meinitcasthavefun@foxmail.com>
2019-09-23 10:34:27 +0800

    生成支付二维码前端实现

---
+++ pinyougou/pinyougou-cart-web/src/main/webapp/js/controller/payController.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        //当前登录用户名
+        username: "",
+        //交易编号
+        outTradeNo: "",
+        //支付总金额
+        totalFee: 0
+    },
+    methods: {
+        //生成支付二维码
+        createNative: function () {
+            this.outTradeNo = this.getParameterByName("outTradeNo");
+            //发送请求；获取信息
+            axios.get("pay/createNative.do?outTradeNo="+this.outTradeNo).then(function (response) {
+                //设置支付总金额
+                app.totalFee = (response.data.totalFee/100).toFixed(2);
+                //生成二维码
+                var qr = new QRious({
+                    element:document.getElementById("qrious"),
+                    level:"M",
+                    size:250,
+                    value: response.data.code_url
+                });
+            });
+
+        },
+        getUsername: function () {
+            axios.get("cart/getUsername.do").then(function (response) {
+                app.username = response.data.username;
+            });
+
+        },
+        //根据参数名字获取参数
+        getParameterByName: function (name) {
+            return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
+        }
+    },
+    created() {
+        this.getUsername();
+        //生成二维码
+        this.createNative();
+    }
+});
